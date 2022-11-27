715b40b9219da04787a42aed8f77e9c74f3b0614
PJ Pollina <contact@pjpollina.info>
2022-11-26 21:05:56 -0500

    Added BLQuickLinks.user.js

---
+++ BLQuickLinks.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Beta Layout Inventory/SDB Quick Links
+// @namespace    https://github.com/pjpollina/neopets-userscripts
+// @version      0.1.0
+// @description  A custom update of Piotr Kardovsky's 2020 Inventory Button
+// @author       PJ Pollina (aeugchad)
+// @match        http*://www.neopets.com/*
+// @icon         https://www.neopets.com//favicon.ico
+// @grant        none
+// ==/UserScript==
+
+// Add necessary CSS
+let css = document.createElement("style")
+css.innerHTML = `
+  .navsub-inv-icon__2020, .navsub-sdb-icon__2020 {
+    width: 25px;
+    height: 25px;
+    margin: auto 4px auto 0;
+    float: left;
+    vertical-align: middle
+   }
+   .navsub-sdb-icon__2020 {
+     background:url(https://images.neopets.com/themes/h5/basic/images/v3/safetydeposit-icon.svg) center center/100% auto no-repeat;
+   }
+   .navsub-inv-icon__2020 {
+     background:url(https://images.neopets.com/themes/h5/basic/images/v3/inventory-icon.svg) center center/100% auto no-repeat;
+   }`;
+document.body.appendChild(css);
+
+// Inject buttons
+let menu = document.querySelector(".navsub-right__2020");
+if(menu) {
+  let inv = document.createElement("a");
+  inv.href = "https://www.neopets.com/inventory.phtml";
+  inv.innerHTML =`
+    <div class="navsub-np-meter__2020" style="display: inline-block; margin-bottom: 0px;">
+	  <div class="navsub-inv-icon__2020"></div>
+		<span class="np-text__2020">Inventory</span>
+	  </div>`;
+  let sdb = document.createElement("a");
+  sdb.href = "https://www.neopets.com/safetydeposit.phtml";
+  sdb.innerHTML =`
+    <div class="navsub-np-meter__2020" style="display: inline-block; margin-bottom: 0px;">
+	  <div class="navsub-sdb-icon__2020"></div>
+		<span class="np-text__2020">SDB</span>
+	  </div>`;
+  menu.prepend(inv);
+  menu.prepend(sdb);
+}
